33a0b2d51ffc3ec85ab24b16411563e6d8053d8b
BKarthik7 <bangikarthik7@gmail.com>
2024-10-30 22:55:31 +0530

    fix recipe

--- recipe.rs
+++ recipe.rs
@@ -1,22 +1,21 @@
-use serde::{Deserialize, Serialize};
+use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-
 pub struct Recipe {
-    pub id: i32,
+    pub id: u32,
     pub name: String,
     pub ingredients: Vec<String>,
-    pub instuctions: Vec<String>,
+    pub instructions: Vec<String>,
     pub servings: u32,
 }
 
 impl Recipe {
-    pub fn new(id: i32, name: String, ingredients: Vec<String>, instuctions: Vec<String>, servings: u32) -> Self {
+    pub fn new(id: u32, name: String, ingredients: Vec<String>, instructions: Vec<String>, servings: u32) -> Self {
         Recipe {
             id,
             name,
             ingredients,
-            instuctions,
+            instructions,
             servings,
         }
     }
